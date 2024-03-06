9c6fd7e39f1afe4c55d52403bf8aa78a88b5a3e6
moisibot <moisesvasquez201930195@cunoc.edu.gt>
2024-03-06 10:13:52 -0600

    con analizadores funcionando

--- src/main/java/Analizadores/Funciones.java
+++ src/main/java/Analizadores/Funciones.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Analizadores;
 
 import Front.Interfaz;
@@ -15,152 +12,93 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.file.Files;
+import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java_cup.runtime.Symbol;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author SELVYN
- */
 public class Funciones {
-    
-      public void analizarLexico() throws IOException {
-        int cont = 1;
 
-        // aque el lexico debe de quitar ese error al generar el AnalizadorLexico . java pero eso hay qye averugyar coomo agregar jar como dependencias 
-        String expr = (String) Ingreso.getText();
+    public void analizarLexico(String expr) throws IOException {
+        int cont = 1;
         AnalizadorLexico lexer = new AnalizadorLexico(new StringReader(expr));
-        String resultado = "LINEA" + cont + "\t\tSIMBOLO\n";
+        StringBuilder resultado = new StringBuilder("LINEA" + cont + "\t\tSIMBOLO\n");
+
         while (true) {
-            Tokens token = lexer. yylex() ;
+            Tokens token = lexer.yylex();
             if (token == null) {
-                ControladorLexico.setText(resultado);
+                ControladorLexico.setText(resultado.toString());
                 return;
             }
-            switch (token) {
-                // esto es la lista para conocer los diferentes tokens que se tienen
-                case Linea:
-                    cont++;
-                    resultado += "Linea" + cont + "\n";
-                case SELECCIONAR:
-                    cont++;
-                    resultado += "<SELECCIONAR>\t" + lexer.lexema + "\n";
-                    break;
-                case EN:
-                    cont++;
-                    resultado += "<EN>\t" + lexer.lexema + "\n";
-                    break;
-                case FILTRAR:
-                    cont++;
-                    resultado += "<FILTRAR>\t" + lexer.lexema + "\n";
-                    break;
-                case INSERTAR:
-                    cont++;
-                    resultado += "<INSERTAR>\t" + lexer.lexema + "\n";
-                    break;
-                case ASIGNAR:
-                    cont++;
-                    resultado += "<ASIGNAR>\t" + lexer.lexema + "\n";
-                    break;
-                case ELIMINAR:
-                    cont++;
-                    resultado += "<ELIMINAR>\t" + lexer.lexema + "\n";
-                    break;
-                case IGUAL:
-                    cont++;
-                    resultado += "<IGUAL>\t" + lexer.lexema + "\n";
-                    break;
-                case MENOR:
-                    cont++;
-                    resultado += "<MENOR>\t" + lexer.lexema + "\n";
-                    break;
-                case MAYOR:
-                    cont++;
-                    resultado += "<MAYOR>\t" + lexer.lexema + "\n";
-                    break;
-                case MENORIGUAL:
-                    cont++;
-                    resultado += "<MENORIGUAL>\t" + lexer.lexema + "\n";
-                    break;
-                case MAYORIGUAL:
-                    cont++;
-                    resultado += "<MAYORIGUAL>\t" + lexer.lexema + "\n";
-                    break;
-                case DIFERENTE:
-                    cont++;
-                    resultado += "<DIFERENTE>\t" + lexer.lexema + "\n";
-                    break;
-                case AND:
-                    cont++;
-                    resultado += "<AND>\t" + lexer.lexema + "\n";
-                    break;
-                case OR:
-                    cont++;
-                    resultado += "<OR>\t" + lexer.lexema + "\n";
-                    break;
-                case IDENTIFICADOR:
-                    cont++;
-                    resultado += "<IDENTIFICADOR>\t" + lexer.lexema + "\n";
-                    break;
-                case NUMERO:
-                    cont++;
-                    resultado += "<NUMERO>\t\t" + lexer.lexema + "\n";
-                    break;
-                case CADENA:
-                    cont++;
-                    resultado += "<CADENA>\t" + lexer.lexema + "\n";
-                    break;
-                case PUNTOCOMA:
-                    cont++;
-                    resultado += "<PUNTOCOMA>\t" + lexer.lexema + "\n";
-                    break;
-                case COMA:
-                    cont++;
-                    resultado += "<COMA>\t\t" + lexer.lexema + "\n";
-                    break;
-                case PARENTESISABIERTO:
-                    cont++;
-                    resultado += "<PARENTESISABIERTO>\t" + lexer.lexema + "\n";
-                    break;
-                case PARENTESISCERRADO:
-                    cont++;
-                    resultado += "<PARENTESISCERRADO>\t" + lexer.lexema + "\n";
-                    break;
-                default:
-                    resultado += "  < " + lexer.lexema + "\n";
-                    break;
-            }
 
+            cont++;
+            resultado.append(getTokenString(token, lexer.lexema));
         }
+    }
 
+    private String getTokenString(Tokens token, String lexema) {
+        switch (token) {
+            case Linea:
+                return "Linea" + "\n";
+            case SELECCIONAR:
+            case EN:
+            case FILTRAR:
+            case INSERTAR:
+            case ASIGNAR:
+            case ELIMINAR:
+            case ACTUALIZAR:
+            case IGUAL:
+            case MENOR:
+            case MAYOR:
+            case MENORIGUAL:
+            case MAYORIGUAL:
+            case DIFERENTE:
+            case AND:
+            case OR:
+            case IDENTIFICADOR:
+            case NUMERO:
+            case CADENA:
+            case PUNTOCOMA:
+            case COMA:
+            case PARENTESISABIERTO:
+            case PARENTESISCERRADO:
+                return "<" + token + ">\t" + lexema + "\n";
+            default:
+                return "  < " + lexema + "\n";
+        }
     }
 
     public void analizar() {
         try {
-            analizarLexico();
+            String expr = Editable.getText();
+            analizarLexico(expr);
         } catch (IOException es) {
             Logger.getLogger(Interfaz.class.getName()).log(Level.SEVERE, null, es);
         }
     }
 
-    public void AnalizadorSintactico() {
-        String entrada = Editable.getText();
-        //este error debera de desaparecer cuando ya se  compilado el sintax eso lo hace en generador
-        Sintactico s = new Sintactico(new Analizadores.LexicoSintac(new StringReader(entrada)));
+   public void AnalizadorSintactico() throws Exception {
+    String entrada = Editable.getText();
+    Sintactico s = new Sintactico(new Analizadores.LexicoSintac(new StringReader(entrada)));
 
-        try {
-            s.parse();
-            ControladorSintactico.setText("Analisis correctamente");
-            ControladorSintactico.setForeground(new Color(26, 111, 61));
-        } catch (Exception ex) {
-            Symbol sym = s.getS();
-            ControladorSintactico.setText("Error de Sintaxis. Linea: " + (sym.right + 1) + "Columna: " + (sym.left + 1) + ", texto: \"" + sym.value + "\"");
-            ControladorSintactico.setForeground(Color.red);
-        }
-
+    try {
+        s.parse();
+        ControladorSintactico.setText("Análisis correcto");
+        ControladorSintactico.setForeground(new Color(26, 111, 61));
+    } catch (ParseException ex) {
+        // Manejo de errores de análisis sintáctico
+        Symbol sym = s.getS();
+        int linea = sym.right + 1;
+        int columna = sym.left + 1;
+        String texto = sym.value.toString();
+        
+        // Actualizar el JLabel para mostrar el error
+        String mensajeError = "Error de Sintaxis. Línea: " + linea + ", Columna: " + columna + ", Texto: \"" + texto + "\"";
+        ControladorSintactico.setText(mensajeError);
+        ControladorSintactico.setForeground(Color.red);
     }
-
 }
+}
+
--- src/main/java/Analizadores/LexicoSintac.jflex
+++ src/main/java/Analizadores/LexicoSintac.jflex
@@ -1,6 +1,6 @@
 package Analizadores;
+import java_cup.runtime.Symbol;
 
-import java_cup.runtime.*;
 
 %%
 
@@ -14,48 +14,47 @@ import java_cup.runtime.*;
 %char
 
 %{
-  
-   private Symbol symbol (int type){
-        return new Symbol(type, yyline,yycolumn);
-   }
-   
+     
    private Symbol symbol (int type, Object value){
         return new Symbol(type, yyline,yycolumn, value);
+   } 
+  private Symbol symbol (int type){
+        return new Symbol(type, yyline,yycolumn);
    }
 %}
 
 %%
 
-"SELECCIONAR"   { return symbol(sym.SELECCIONAR); }
-"EN"            { return symbol(sym.EN); }
-"FILTRAR"       { return symbol(sym.FILTRAR); }
-"INSERTAR"      { return symbol(sym.INSERTAR); }  
-"ASIGNAR"       { return symbol(sym.ASIGNAR); }
-"ELIMINAR"      { return symbol(sym.ELIMINAR); }
-"ACTUALIZAR"    { return symbol(sym.ACTUALIZAR); }
+"SELECCIONAR"   { return new Symbol(sym.SELECCIONAR,yychar, yyline, yytext()); }
+"EN"            { return new Symbol(sym.EN,yychar, yyline,yytext()); }
+"FILTRAR"       { return new Symbol(sym.FILTRAR,yychar, yyline,yytext()); }
+"INSERTAR"      { return new Symbol(sym.INSERTAR,yychar, yyline,yytext()); }  
+"ASIGNAR"       { return new Symbol(sym.ASIGNAR,yychar, yyline, yytext()); }
+"ELIMINAR"      { return new Symbol(sym.ELIMINAR,yychar, yyline,yytext()); }
 
-"="             { return symbol(sym.IGUAL); }
-"<"             { return symbol(sym.MENOR); }
-">"             { return symbol(sym.MAYOR); }
-"<="            { return symbol(sym.MENORIGUAL); }
-">="            { return symbol(sym.MAYORIGUAL); }
-"<>"            { return symbol(sym.DIFERENTE); }
+"="             { return new Symbol(sym.IGUAL,yychar, yyline, yytext()); }
+"<"             { return new Symbol(sym.MENOR,yychar, yyline, yytext()); }
+">"             { return new Symbol(sym.MAYOR,yychar, yyline, yytext()); }
+"<="            { return new Symbol(sym.MENORIGUAL,yychar, yyline, yytext()); }
+">="            { return new Symbol(sym.MAYORIGUAL,yychar, yyline, yytext()); }
+"<>"            { return new Symbol(sym.DIFERENTE,yychar, yyline, yytext()); }
 
-"AND"           { return symbol(sym.AND); }
-"OR"            { return symbol(sym.OR); }
+"AND"           { return new Symbol(sym.AND,yychar, yyline, yytext()); }
+"OR"            { return new Symbol(sym.OR,yychar, yyline, yytext()); }
 
-[A-Za-z]+[A-Za-z0-9_]* { return symbol(sym.IDENTIFICADOR); }
+[A-Za-z]+[A-Za-z0-9_]* { return new Symbol(sym.IDENTIFICADOR,yychar, yyline, yytext()); }
 
-[0-9]+          { return symbol(sym.NUMERO); }
-\"[^\"]*\"      { return symbol(sym.CADENA); }
+
+[0-9]+          { return new Symbol(sym.NUMERO,yychar, yyline, yytext()); }
+\"[^\"]*\"      { return new Symbol(sym.CADENA,yychar, yyline, yytext()); }
 
 [ \t\n\r\f]+    { /* Ignorar espacios en blanco */ }
 "//" .*         { /* Ignorar comentarios de una línea */ }
-"/*" [^*]* "*" ~"*/"  { /* Ignorar comentarios de varias líneas */ }
+"/" [^]* "" ~"/"  { /* Ignorar comentarios de varias líneas */ }
 
-";"             { return symbol(sym.PUNTOCOMA); }
-","             { return symbol(sym.COMA); }
-"("             { return symbol(sym.PARENTESISABIERTO); }
-")"             { return symbol(sym.PARENTESISCERRADO); }
+";"             { return new Symbol(sym.PUNTOCOMA,yychar, yyline, yytext()); }
+","             { return new Symbol(sym.COMA,yychar, yyline, yytext()); }
+"("             { return new Symbol(sym.PARENTESISABIERTO,yychar, yyline, yytext()); }
+")"             { return new Symbol(sym.PARENTESISCERRADO,yychar, yyline, yytext()); }
 
-.               { /* Manejar caracteres no reconocidos */ }
+[^] {}
